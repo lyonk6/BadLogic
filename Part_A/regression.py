5e0b85803a23d949d83b9cfba4055d0698f00e82
Ken Lyon <fatstrat389@gmail.com>
2020-04-18 00:59:01 -0700

    Completed the regression step.

--- Part_A/regression.py
+++ Part_A/regression.py
@@ -1,13 +1,11 @@
-OAimport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import tools
 
-################################################################################    
 """ In this app, we train a very simple neural network to modify a given input 
     by a factor of phi. Use the ratio of neighbor Fibonacci numbers to create 
     increasing aproximations of phi as training data. Then plot what we find!"""
-
 
 """ Start by creating our training data. Create corresponding torch vectors for
     this imput data: """
@@ -24,7 +22,6 @@ for i in range(1, N+1):
     x.append(i)
     y.append(i* fn/f0)
 
-
 # Reshape x and y before making tensors [1, 2, ...] -> [[1], [2], ...]]
 xA = np.asarray(x, dtype=np.float32).reshape(1, N)
 yA = np.asarray(y, dtype=np.float32).reshape(1, N)
@@ -34,23 +31,11 @@ yT = torch.from_numpy(np.transpose(yA))
 ################################################################################    
 
 """ Now let's define our very simple network of just a single node. Also we will
-    define our learning rate. """
+    define our learning rate. 
 
-""" Later we train our neural network on the training data treating X_torch
-    as the input and Y_torch as the desired output.  Note that with a
-    learning_rate of 1e-5 it takes at least 1000 iterations to see the
-    regression line budge. Consider alernate learning functions.
-
-    Additionally, we will define vectors to track how w1, w2, y_pred
-    and the gradients change.
-
-    Finally, note the datatypes of w1, w2, y_pred is <class 'torch.Tensor'>
-    so it must be converted back to a numpy array before plotting.
+    Note the datatypes of w1, w2, y_pred is <class 'torch.Tensor'>    
     """
 
-
-
-B
 learning_rate = 1e-5   
 w1 = torch.zeros(1, 1, requires_grad=True)
 w2 = torch.zeros(1, 1, requires_grad=True)
@@ -79,15 +64,14 @@ for i in range(1, 2000):
     # Perform the backward pass
     loss.backward()
 
-    """ Here we actually adjust the network. Note that we turn off gradient 
-        tracking This time however we are not 
-        recording the operations we perform.
+    """ Here we actually adjust the network. Note that we turn off autograd. 
+        """
+    
     with torch.no_grad():
         w1 -= learning_rate * w1.grad + learning_rate
         w2 -= learning_rate * w2.grad + learning_rate
         w1.grad.zero_()
         w2.grad.zero_()
-
 
 final_pass = xT.mm(w1).mm(w2)
 v = final_pass.detach().numpy()
@@ -95,3 +79,12 @@ v = final_pass.detach().numpy()
 """ Note that xT and yT do no change. xT is always the input and yT is the 
     expected output.
     """
+""" Next plot the data as a scatterplot and add our regression line too. """  
+plt.figure(figsize=(12, 8))
+plt.scatter(x, y, label='Original data', s=250, c='g')    
+plt.plot(x, v, label = 'Fitted line')
+plt.plot(x, p001, label = 'p001')
+plt.plot(x, p500, label = 'p500')
+plt.plot(x, p600, label = 'p600')
+plt.legend()  
+plt.show()    
