16458b39d91476b4927474114a41add4fbbfeff4
lyonk6 <fatstrat389@gmail.com>
2020-11-08 00:27:19 -0800

    Added notes.

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -25,4 +25,16 @@ y_pred = ""
 predictions = []
 
 
-#for i in range(1, 2000):
+for i in range(1, 2000):
+    # grab a bit of training data:
+
+
+    # perform the forward pass
+    y_pred = xT.mm(w1).mm(w2)
+
+    if i == 1:
+        print("No fucking way man!")
+        print("y_pred is a : ", type(y_pred))
+        print("infact, here is y_pred: ", y_pred)
+        print("Here is w1: ", w1)
+        print("Here is w2: ", w2)
--- regression/regression.py
+++ regression/regression.py
@@ -3,8 +3,8 @@ import numpy as np
 import torch
 import tools
 
-""" In this app, we train a very simple neural network to modify a given input 
-    by a factor of phi. Use the ratio of neighbor Fibonacci numbers to create 
+""" In this app, we train a very simple neural network to modify a given input
+    by a factor of phi. Use the ratio of neighbor Fibonacci numbers to create
     increasing aproximations of phi as training data. Then plot what we find!"""
 
 """ Start by creating our training data. Create corresponding torch vectors for
@@ -29,16 +29,16 @@ xT = torch.from_numpy(np.transpose(xA))
 yT = torch.from_numpy(np.transpose(yA))
 
 #print("Here is xT: ", xT) [1
-#print("Here is yT: ", yT) 
-################################################################################    
+#print("Here is yT: ", yT)
+################################################################################
 
 """ Now let's define our very simple network of just a single node. Also we will
-    define our learning rate. 
+    define our learning rate.
 
-    Note the datatypes of w1, w2, y_pred is <class 'torch.Tensor'>    
+    Note the datatypes of w1, w2, y_pred is <class 'torch.Tensor'>
     """
 
-learning_rate = 1e-5   
+learning_rate = 1e-5
 w1 = torch.zeros(1, 1, requires_grad=True)
 w2 = torch.zeros(1, 1, requires_grad=True)
 
@@ -50,21 +50,6 @@ for i in range(1, 2000):
 
     # perform the forward pass
     y_pred = xT.mm(w1).mm(w2)
-    
-    if i == 1:
-        p001 = y_pred.detach().numpy()
-        #Note that y_pred, xT, w1, w2 are all tensors.       
-        print("infact, here is y_pred: ", y_pred)
-        print("and, here is xT: ", xT)
-        print("and, here is w1: ", w1)
-        print("and, here is w2: ", w2)
-        
-        
-    if i == 500:
-        p500 = y_pred.detach().numpy()
-
-    if i == 600:
-        p600 = y_pred.detach().numpy()
 
     # calculate the error
     loss = (y_pred - yT).pow(2).sum()
@@ -72,9 +57,30 @@ for i in range(1, 2000):
     # Perform the backward pass
     loss.backward()
 
-    """ Here we actually adjust the network. Note that we turn off autograd. 
+    if i == 1:
+        p001 = y_pred.detach().numpy()
+        #Note that y_pred, xT, w1, w2 are all tensors.
+        print("infact, here is y_pred: ", y_pred)
+        print("and, here is xT: ", xT)
+        print("and, here is yT: ", yT)
+        print("and, here is w1: ", w1)
+        print("and, here is w2: ", w2)
+        print("and, here's the loss: ", loss)
+        print("and, here's the type of the loss: ", type(loss))
+
+
+    if i == 500:
+        p500 = y_pred.detach().numpy()
+
+    if i == 600:
+        p600 = y_pred.detach().numpy()
+
+
+
+
+    """ Here we actually adjust the network. Note that we turn off autograd.
         """
-    
+
     with torch.no_grad():
         w1 -= learning_rate * w1.grad + learning_rate
         w2 -= learning_rate * w2.grad + learning_rate
@@ -83,19 +89,19 @@ for i in range(1, 2000):
 
 final_pass = xT.mm(w1).mm(w2)
 v = final_pass.detach().numpy()
-################################################################################    
-""" Note that xT and yT do no change. xT is always the input and yT is the 
+################################################################################
+""" Note that xT and yT do no change. xT is always the input and yT is the
     expected output.
     """
-""" Next plot the data as a scatterplot and add our regression line too. """  
+""" Next plot the data as a scatterplot and add our regression line too. """
 plt.figure(figsize=(12, 8))
-plt.scatter(x, y, label='Original data', s=250, c='g')    
+plt.scatter(x, y, label='Original data', s=250, c='g')
 plt.plot(x, v, label = 'Fitted line')
 plt.plot(x, p001, label = 'p001')
 plt.plot(x, p500, label = 'p500')
 plt.plot(x, p600, label = 'p600')
-plt.legend()  
-plt.show()    
+plt.legend()
+plt.show()
 
 print("Finally, here are w1 and w2: ")
 print("\tw1: ", w1[0])
