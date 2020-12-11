f5ab458276b010d5c1f6e11f5b220082322ccb68
Ken Lyon <fatstrat389@gmail.com>
2020-12-10 19:58:01 -0800

    refactored and cleaned up a bit.

--- regression/tools.py
+++ regression/tools.py
@@ -2,10 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_regression(x, y, v=[0], plot_line=False):
-    """ Next plot the data as a scatterplot and add our regression line too. """  
+    """ Next plot the data as a scatterplot and add our regression line too. """
     plt.figure(figsize=(12, 8))
-    plt.scatter(x, y, label='Original data', s=250, c='g')    
+    plt.scatter(x, y, label='Original data', s=250, c='g')
     if plot_line and len(v) == len(x):
         plt.plot(x, v, label = 'Fitted line')
-    plt.legend()  
-    plt.show()    
+    plt.legend()
+    plt.show()
+
+def shout(p, yp, v1, v2):
+    print("shout ", p)
+    print("  y_pred is:\n", yp)
+    print("  Here is v1: ", v1)
+    print("  Here is v2: ", v2)
+    #print("  Here is v2: ", v2)
+    print("")
