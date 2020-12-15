5657bc2288ae1ac848346955c9d6354ef7dbd0d0
Ken Lyon <fatstrat389@gmail.com>
2020-12-14 17:29:08 -0800

    Added updated polynomial to have two hidden layers.

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -35,15 +35,13 @@ output_size = 1
 
 learning_rate = 1e-7
 w1 = torch.rand(input_size, hidden_size, dtype=torch.float64, requires_grad=True)
-wh = torch.rand(hidden_size, hidden_size, dtype=torch.float64, requires_grad=True)
+wh1 = torch.rand(hidden_size, hidden_size, dtype=torch.float64, requires_grad=True)
+wh2 = torch.rand(hidden_size, hidden_size, dtype=torch.float64, requires_grad=True)
 w2 = torch.rand(hidden_size, output_size, dtype=torch.float64, requires_grad=True)
-
-print("Here is a tensor:       ", w1)
-print("Here is another tensor: ", w2)
 
 y_pred = ""
 
-for i in range(1, 20000):
+for i in range(1, 200000):
     xT, yT = random_poly_sample(f['x^4'],
                                 f['x^3'],
                                 f['x^2'],
@@ -51,7 +49,7 @@ for i in range(1, 20000):
                                 f['c'])
 
     # perform the forward pass
-    y_pred = xT.mm(w1).mm(wh).mm(w2)
+    y_pred = xT.mm(w1).mm(wh1).mm(wh2).mm(w2)
 
     # calculate the error
     loss = (y_pred - yT).pow(2).sum()
@@ -59,23 +57,19 @@ for i in range(1, 20000):
     # Perform the backward pass
     loss.backward()
 
-    if i < 20 or i==500 or i==5000:
-        shout(i, y_pred, w1, wh, w2)
+    if i < 20 or i==500 or i==5000 or i==50000:
+        shout(i, y_pred, w1, wh1, w2)
 
     with torch.no_grad():
-        w1 -= learning_rate * w1.grad - learning_rate
-        wh -= learning_rate * wh.grad - learning_rate
-        w2 -= learning_rate * w2.grad - learning_rate
+        w1  -= learning_rate * w1 .grad - learning_rate
+        wh1 -= learning_rate * wh1.grad - learning_rate
+        wh2 -= learning_rate * wh2.grad - learning_rate
+        w2  -= learning_rate * w2 .grad - learning_rate
         w1.grad.zero_()
-        wh.grad.zero_()
+        wh1.grad.zero_()
+        wh2.grad.zero_()
         w2.grad.zero_()
 
-
-final_pass = xT.mm(w1).mm(w2)
-v = final_pass.detach().numpy()
-
-print("Here is our final pass: ", final_pass)
-print("Here is v whatever it is:", v[0][0])
 
 plt = make_plot(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'])
 
@@ -89,7 +83,7 @@ for k in x:
     #y[i]= get_y_value(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'], k)
     # Use the model to make a prediction:
     xK = torch.tensor([[k]], dtype=torch.float64)
-    y_pred = xK.mm(w1).mm(wh).mm(w2)
+    y_pred = xK.mm(w1).mm(wh1).mm(wh2).mm(w2)
     y[i]=y_pred.detach().numpy()
     i=i+1
 
