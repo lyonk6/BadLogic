d5243a9f5af62a05586b02d68faa438637ef2166
Ken Lyon <fatstrat389@gmail.com>
2021-01-16 15:25:07 -0800

    Created data.py that generates source plots.

--- waves/data.py
+++ waves/data.py
@@ -1,10 +1,15 @@
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
-MIN = 10.0
-MAX = 10.0
+MIN = -10
+MAX = 10
 
 # Let a, b, c be constants > 0 for the function:
-#  Z = (A**2) * (X**2) + (B**2) * (Y**2) - c
+#  Z = (a**2) * (X**2) + (b**2) * (Y**2) - c
 def sample_paraboloid(a = 0.25, b = 0.25, c = 5):
     x = random.uniform(MIN, MAX)
     y = random.uniform(MIN, MAX)
@@ -12,7 +17,7 @@ def sample_paraboloid(a = 0.25, b = 0.25, c = 5):
     return x, y, z
 
 # Let a, b, c be constants > 0 for the function:
-#  Z = (A**2) * (X**2) - (B**2) * (Y**2) - c
+#  Z = (a**2) * (X**2) - (b**2) * (Y**2) - c
 def sample_saddle(a = 0.25, b = 0.25, c = 5):
     x = random.uniform(MIN, MAX)
     y = random.uniform(MIN, MAX)
@@ -27,8 +32,55 @@ def sample_waves():
     z = np.sin(r)
     return x, y, z
 
+# Define X, Y and Z vectors suitable for a paraboloid.
+def plot_paraboloid(a = 0.25, b = 0.25, c = 5):
+    # Define X, Y, and Z:
+    X = np.arange(MIN, MAX, 0.25)
+    Y = np.arange(MIN, MAX, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = (a**2) * (X**2) + (b**2) * (Y**2) - c
+    return plot_surface(X, Y, Z)
 
+# Define X, Y and Z vectors suitable for a saddle paraboloid.
+def plot_saddle(a = 0.25, b = 0.25, c = 3):
+    # Define X, Y, and Z:
+    X = np.arange(MIN, MAX, 0.25)
+    Y = np.arange(MIN, MAX, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = (a**2) * (X**2) - (b**2) * (Y**2) - c
+    return plot_surface(X, Y, Z)
 
+# Define X, Y and Z vectors that map an expanding sine wave.
+def plot_waves():
+    # Make data.
+    X = np.arange(MIN, MAX, 0.25)
+    Y = np.arange(MIN, MAX, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
+    return plot_surface(X, Y, Z)
+
+def plot_surface(X, Y, Z):
+    # Define the figure:
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+    linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(MIN - 0.01, MAX + 0.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    return plt
 
 if __name__ == '__main__':
-    print("data ")
+    print("Here in data:")
+    plot_waves().show()
+    plot_saddle().show()
+    plot_paraboloid().show()
