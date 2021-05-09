39b889ce5beb656ec7138df2f47193d387280321
lyonk6 <fatstrat389@gmail.com>
2021-05-08 17:46:24 -0700

    Added polynomial map.

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -2,18 +2,11 @@
 import numpy as np
 import tools
 import torch
-
+from torch import nn
 from tools import random_poly_sample, shout, make_plot, get_y_value
-""" Previously, we trained a model to transform an input by a factor of phi.
-    Here we will create a more complex network to fit a polynomial. Then we
-    figure out how to plot it. """
-
-""" Instead of creating tensors of training data, we will use random values
-    generated from a given polynomial function. """
-
 
 ################################################################################
-print("Start")
+
 
 """ Before we used the variables x and y to represent the training input and
     output. We also defined x = {1-11} and y as a transformation proportional
@@ -22,71 +15,33 @@ print("Start")
 """ Here we will use randomly generated x and y values defined from a given
     function to generate our training data. """
 # 1, -40, -100, 1, 0
-f = {
-    "x^4" :    1,
-    "x^3" :  -40,
-    "x^2" : -100,
-    "x"   :    1,
-    "c"   :    0
-}
-input_size  = 1
-hidden_size = 10
-output_size = 1
 
-learning_rate = 1e-7
-w1 = torch.rand(input_size, hidden_size, dtype=torch.float64, requires_grad=True)
-wh1 = torch.rand(hidden_size, hidden_size, dtype=torch.float64, requires_grad=True)
-wh2 = torch.rand(hidden_size, hidden_size, dtype=torch.float64, requires_grad=True)
-w2 = torch.rand(hidden_size, output_size, dtype=torch.float64, requires_grad=True)
+class Polynomial(nn.Module):
+    def __init__(self, input_size=256, hidden_size=10, output_size=1):
+        super().__init__()
+        self.input_layer   = nn.Parameter(torch.rand(input_size, hidden_size, dtype=torch.float64))
+        self.hidden_layer  = nn.Parameter(torch.rand(hidden_size, hidden_size, dtype=torch.float64))
+        self.output_layer  = nn.Parameter(torch.rand(hidden_size, output_size, dtype=torch.float64))
 
-y_pred = ""
+        # What are our inputs?
 
-for i in range(1, 200000):
-    xT, yT = random_poly_sample(f['x^4'],
-                                f['x^3'],
-                                f['x^2'],
-                                f['x'],
-                                f['c'])
+    def forward(self, input):
+        return (input @ self.input_layer @ self.hidden_layer @ self.output_layer)
 
-    # perform the forward pass
-    y_pred = xT.mm(w1).mm(wh1).mm(wh2).mm(w2)
+if __name__ == '__main__':
+    print("Start")
 
-    # calculate the error
-    loss = (y_pred - yT).pow(2).sum()
+    # Part 1:
+    ## { Create Model, Create Optimizer, Create Loop}
+    model = Polynomial()
+    optimizer = torch.optim.Adam(model.parameters(), lr= 1e-4)
 
-    # Perform the backward pass
-    loss.backward()
+    for i in range(1000):
+        if i % 100 == 0:
+            print("{} iterations".format(i))
 
-    if i < 20 or i==500 or i==5000 or i==50000:
-        shout(i, y_pred, w1, wh1, w2)
+        # Part 2:
+        ## {forward pass, loss, backward pass}
 
-    with torch.no_grad():
-        w1  -= learning_rate * w1 .grad - learning_rate
-        wh1 -= learning_rate * wh1.grad - learning_rate
-        wh2 -= learning_rate * wh2.grad - learning_rate
-        w2  -= learning_rate * w2 .grad - learning_rate
-        w1.grad.zero_()
-        wh1.grad.zero_()
-        wh2.grad.zero_()
-        w2.grad.zero_()
-
-
-plt = make_plot(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'])
-
-x = np.linspace(tools.MIN, tools.MAX, 256, endpoint = True)
-y = np.empty([256])
-#print("x is a: ", y)
-#print("x is a: ", x)
-
-i=0
-for k in x:
-    #y[i]= get_y_value(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'], k)
-    # Use the model to make a prediction:
-    xK = torch.tensor([[k]], dtype=torch.float64)
-    y_pred = xK.mm(w1).mm(wh1).mm(wh2).mm(w2)
-    y[i]=y_pred.detach().numpy()
-    i=i+1
-
-#print("Here is y again:", y)
-plt.plot(x, y, label = 'prediction')
-plt.show()
+        # part 3:
+        ## {zero_grad(), loss.backward(), optimizer.step()}
