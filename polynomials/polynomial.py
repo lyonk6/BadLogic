a4e77d8880188751567d90efe2d31b1d954fa4d9
lyonk6 <fatstrat389@gmail.com>
2021-05-09 19:18:55 -0700

    Adding input data.

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -3,10 +3,16 @@ import numpy as np
 import tools
 import torch
 from torch import nn
-from tools import random_poly_sample, shout, make_plot, get_y_value
+from tools import wide_poly_sample 
 
 ################################################################################
-
+f = {
+    "x^4" :    1,
+    "x^3" :  -40,
+    "x^2" : -100,
+    "x"   :    1,
+    "c"   :    0
+}
 
 """ Before we used the variables x and y to represent the training input and
     output. We also defined x = {1-11} and y as a transformation proportional
@@ -40,8 +46,11 @@ if __name__ == '__main__':
         if i % 100 == 0:
             print("{} iterations".format(i))
 
+
         # Part 2:
         ## {forward pass, loss, backward pass}
+        xT, yT = wide_poly_sample(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'])
+        output = model(xT)
 
         # part 3:
         ## {zero_grad(), loss.backward(), optimizer.step()}
