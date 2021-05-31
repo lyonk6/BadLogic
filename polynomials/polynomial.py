48dc62dfb0722fe1af9fb346decf46806d39b77d
Ken Lyon <fatstrat389@gmail.com>
2021-05-31 14:48:49 -0700

    Adding print statements to debug

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -23,7 +23,7 @@ f = {
 # 1, -40, -100, 1, 0
 
 class Polynomial(nn.Module):
-    def __init__(self, input_size=256, hidden_size=10, output_size=1):
+    def __init__(self, input_size=256, hidden_size=4, output_size=1):
         super().__init__()
         self.input_layer   = nn.Parameter(torch.rand(input_size, hidden_size, dtype=torch.float64))
         self.hidden_layer  = nn.Parameter(torch.rand(hidden_size, hidden_size, dtype=torch.float64))
@@ -41,12 +41,13 @@ if __name__ == '__main__':
     ## { Create Model, Create Optimizer, Create Loop}
     model = Polynomial()
     optimizer = torch.optim.Adam(model.parameters(), lr= 1e-4)
+    
     for i in range(1000):
         # Part 2:
         ## {forward pass, loss, backward pass}
         xT, yT = wide_poly_sample(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'])
         output = model(xT)
-        loss   = torch.abs(yT - output)
+        loss   = torch.abs(output)
 
         # part 3:
         ## {zero_grad(), loss.backward(), optimizer.step()}
@@ -55,10 +56,15 @@ if __name__ == '__main__':
         optimizer.step()
 
         # Print an update
-        if i % 100 == 0:
-            print("{} iterations".format(i))
+        if i % 250 == 0:
+            print("\n{} iterations".format(i))
+            print("  Loss       = {} ".format(loss))
+            print("  Output     = {} ".format(output))
+            print("hidden layer = \n{} ".format(model.hidden_layer))
+            print("hidden grad  = \n{} ".format(model.hidden_layer.grad))
 
-
-
+    x = np.linspace(tools.MIN, tools.MAX, tools.IMAGE_WIDTH, endpoint = True)
+    y = np.zeros([tools.IMAGE_WIDTH])
     plt = make_plot(f['x^4'], f['x^3'], f['x^2'], f['x'], f['c'])
+    plt.plot(x, y, label = 'prediction')
     plt.show()
