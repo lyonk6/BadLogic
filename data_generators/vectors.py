4a4141ce784462160b3e7d0cb437835bb51304a3
Ken Lyon <fatstrat389@gmail.com>
2022-05-08 22:11:33 -0700

    Added function to append a ScatterVector.

--- data_generators/vectors.py
+++ data_generators/vectors.py
@@ -6,24 +6,43 @@ class ScatterVectors:
             self.dim   = len(center)
             if self.dim != len(sigmas):
                 raise ValueError("Parameter 'sigmas' must be the same size as 'center'.")
-            self.array = np.empty([self.dim, length], type)
+            self.arr = np.empty([self.dim, length], type)
 
             for i in range(self.dim):
-                self.array[i] = np.random.normal(center[i], sigmas[i], length)
+                self.arr[i] = np.random.normal(center[i], sigmas[i], length)
 
         except ValueError as err:
             print(err)
         except TypeError as err:
             print(err)
 
-    def getArray(self):
-        return self.array
+    def array(self):
+        return self.arr
+
+    def append(self, other):
+        try:
+            print(type(self.arr.dtype))
+            print(type(other.arr.dtype))
+            if self.dim != other.dim:
+                raise ValueError("Error. ScatterVectors have incompatable dimensions.")
+            print("check!")
+            # todo merge two arrays:
+            self.arr = np.append(self.arr, other.arr, axis=1)
+            return self
+        except ValueError as err:
+            print(err)
+        except TypeError as err:
+            print(err)
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    length = 1000
+    length = 10
     sv = ScatterVectors([0, 0, 0], [0.1, 0.2, 0.3], length)
-
-    plt.scatter(sv.getArray()[0],sv.getArray()[2], alpha=0.5)
+    sv2= ScatterVectors([2, 2, 2], [0.3, 0.6, 0.1], length)
+    sv3= sv2.append(sv)
+    plt.scatter(sv3.array()[0],sv3.array()[2], alpha=0.5)
+    print(sv.array().shape)
+    print(sv2.array().shape)
+    print(sv3.array().shape)
     plt.show()
