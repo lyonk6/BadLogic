079ca5b5b996c70a1ab9b522ad2be7f6e0a4381f
Ken Lyon <fatstrat389@gmail.com>
2023-02-07 13:53:29 -0800

    refactored and added a trivial 'proof'

---
+++ maths/proofs/rna_numbers.py
@@ -0,0 +1,22 @@
+import math
+
+def rna_symbol(n):
+    if n == 0:
+        return 'A'
+    if n == 1:
+        return 'C'
+    if n == 2:
+        return 'G'
+    if n == 3:
+        return 'U'
+    if n >=4 :
+        return 'X'
+
+def number_conversion(n):
+    n=n-1
+    if n < 4:
+        return rna_symbol(n)
+    else:
+        next = n // 4
+        #return number_conversion(next) + rna_symbol(n % 4)
+        return number_conversion(next) + rna_symbol(n - next)
