33c295910e53feaf1d8aec4ce3148e5b1b1b051d
lyonk6 <fatstrat389@gmail.com>
2023-02-24 11:23:43 -0800

    added amino-acid sequence conversion.

--- maths/proofs/rna_numbers.py
+++ maths/proofs/rna_numbers.py
@@ -1,24 +1,33 @@
-def rna_symbol(n):
-    if n == 0:
-        return 'A'
-    if n == 1:
-        return 'C'
-    if n == 2:
-        return 'G'
-    if n == 3:
-        return 'U'
-    if n >=4 :
-        return 'X'
-
 def digits_to_rna_sequence(n):
     n=n-1
+    nucleotides = ['A', 'C', 'G', 'U']
     if n < 4:
-        return rna_symbol(n)
+        return nucleotides[n]
     else:
-        return digits_to_rna_sequence(n // 4) + rna_symbol(n % 4)
+        return digits_to_rna_sequence(n // 4) + nucleotides[n % 4]
 
-
+def digits_to_peptide_sequence(n):
+    n=n-1
+    amino_acids = [
+        'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 
+        'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+    if n < 20:
+        return amino_acids[n]
+    else:
+        return digits_to_peptide_sequence(n // 20) + amino_acids[n % 20]
 
 if __name__ == "__main__":
+    print("RNAs:")
     for x in range(1,10):
         print(digits_to_rna_sequence(x))
+    print("\nAmino acids:")
+    print(digits_to_peptide_sequence(20))
+    print(digits_to_peptide_sequence(40))
+    print(digits_to_peptide_sequence(60))
+    print(digits_to_peptide_sequence(80))
+    print(digits_to_peptide_sequence(100))
+    print(digits_to_peptide_sequence(200))
+    print(digits_to_peptide_sequence(300))
+    print(digits_to_peptide_sequence(400))
+    for x in range(395, 405):
+        print(digits_to_peptide_sequence(x))
