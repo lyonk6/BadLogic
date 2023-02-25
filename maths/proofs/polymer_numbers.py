79acbee68546e42eea5539e4956c2931663b8721
lyonk6 <fatstrat389@gmail.com>
2023-02-24 18:37:18 -0800

    even better

---
+++ maths/proofs/polymer_numbers.py
@@ -0,0 +1,23 @@
+def digits_to_sequence(n, symbol_list):
+    num_symbols = len(symbol_list)
+    if n < num_symbols:
+        return symbol_list[n]
+    else:
+        return digits_to_sequence((n // num_symbols)-1, symbol_list) + symbol_list[n % num_symbols]
+
+
+if __name__ == "__main__":
+    nucleotides = 'ACGU'
+    amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
+    print("\nAmino acids:")
+    for x in range(0, 21):
+        print(digits_to_sequence(x, amino_acids))
+
+    print("...")
+    for x in range(399, 402):
+        print(digits_to_sequence(x, amino_acids))
+
+    print("RNAs:")
+    for x in range(0,21):
+        print(digits_to_sequence(x, nucleotides))
+    print(digits_to_sequence(43, nucleotides))
