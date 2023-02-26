a611e910830dd29f8c7b7462e4eef03431b6512c
lyonk6 <fatstrat389@gmail.com>
2023-02-25 16:22:43 -0800

    Added code to reverse mapping of numbers.

--- maths/proofs/polymer_numbers.py
+++ maths/proofs/polymer_numbers.py
@@ -6,18 +6,40 @@ def digits_to_sequence(n, symbol_list):
         return digits_to_sequence((n // num_symbols)-1, symbol_list) + symbol_list[n % num_symbols]
 
 
+def sequence_to_digits(sequence, symbol_map):
+    print(sequence[:1], sequence)
+
+    if len(sequence) <= 1:
+        return symbol_map[sequence]
+    else:
+        power = (len(symbol_map) ** (len(sequence)-1))
+        power_sum = power + power * symbol_map[sequence[:1]]
+        print("(len(symbol_map) ** len(sequence))", len(symbol_map),  len(sequence), power, power_sum)
+        return sequence_to_digits(sequence[1:], symbol_map) + power_sum
+
+
+def make_symbol_map(symbol_list):
+    symbol_map = {}
+    for i, x in enumerate(symbol_list):
+        print(i, " ", x)
+        symbol_map[x] = i
+    return symbol_map
+
 if __name__ == "__main__":
     nucleotides = 'ACGU'
     amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
-    print("\nAmino acids:")
-    for x in range(0, 21):
-        print(digits_to_sequence(x, amino_acids))
+#    print("\nAmino acids:")
+#    for x in range(0, 21):
+#        print(digits_to_sequence(x, amino_acids))
+#
+#    print("...")
+#    for x in range(419, 422):
+#        print(digits_to_sequence(x, amino_acids))
+#
+#    print("RNAs:")
+#    for x in range(0,21):
+#        print(digits_to_sequence(x, nucleotides))
+#    print(digits_to_sequence(43, nucleotides))
 
-    print("...")
-    for x in range(419, 422):
-        print(digits_to_sequence(x, amino_acids))
-
-    print("RNAs:")
-    for x in range(0,21):
-        print(digits_to_sequence(x, nucleotides))
-    print(digits_to_sequence(43, nucleotides))
+    print(sequence_to_digits(nucleotides, make_symbol_map(nucleotides)))
+    #print(digits_to_sequence(111, nucleotides))
