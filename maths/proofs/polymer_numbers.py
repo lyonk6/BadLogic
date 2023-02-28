af2f028b08901596330213670b29aa3a67b03d6a
lyonk6 <fatstrat389@gmail.com>
2023-02-28 15:58:41 -0800

    even better.

--- maths/proofs/polymer_numbers.py
+++ maths/proofs/polymer_numbers.py
@@ -7,34 +7,33 @@ def digits_to_sequence(n, symbol_list):
 
 
 def sequence_to_digits(sequence, symbol_map):
-    print(sequence[:1], sequence)
-
     if len(sequence) <= 1:
         return symbol_map[sequence]
     else:
         power = (len(symbol_map) ** (len(sequence)-1))
         power_sum = power + power * symbol_map[sequence[:1]]
-        print("(len(symbol_map) ** len(sequence))", len(symbol_map),  len(sequence), power, power_sum)
         return sequence_to_digits(sequence[1:], symbol_map) + power_sum
 
 
 def make_symbol_map(symbol_list):
     symbol_map = {}
     for i, x in enumerate(symbol_list):
-        print(i, " ", x)
         symbol_map[x] = i
     return symbol_map
 
 if __name__ == "__main__":
     nucleotides = 'ACGU'
     amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
-#    print("\nAmino acids:")
-#    for x in range(0, 21):
-#        print(digits_to_sequence(x, amino_acids))
-#
-#    print("...")
-#    for x in range(419, 422):
-#        print(digits_to_sequence(x, amino_acids))
+    print("Count to 20:")
+    for x in range(0, 21):
+        print(sequence_to_digits(digits_to_sequence(x, amino_acids), make_symbol_map(amino_acids)))
+
+    print("\nAmino acids:")
+    for x in range(0, 21):
+        print(digits_to_sequence(x, amino_acids))
+    print("...")
+    for x in range(419, 422):
+        print(digits_to_sequence(x, amino_acids))
 #
 #    print("RNAs:")
 #    for x in range(0,21):
