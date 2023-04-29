edfd90baafc36bc3c56466a9fc6cd812857aae0f
lyonk6 <fatstrat389@gmail.com>
2023-04-29 16:47:08 -0700

    refactored

--- backprop/mnist.py
+++ backprop/mnist.py
@@ -8,7 +8,7 @@ class training_data:
 
     def pop_image(self):
         self.index = self.index + 1
-        return self.images.iloc[self.index -1]
+        return self.images.iloc[self.index -1].to_numpy()
 
     def has_next_image(self):
         # The number of rows in a pandas table is given by: df.shape[0]
@@ -18,20 +18,26 @@ class training_data:
             return False
 
 
-def convert_and_reshape(pandas_series):
-    return pandas_series.to_numpy().reshape((28,28))
+def convert_and_reshape(numpy_array):
+    return numpy_array[1:].reshape((28,28))
 
-if __name__ == "__main__":
-    d = training_data()
-    d.pop_image()
-    image = convert_and_reshape(d.pop_image()[1:])
+def plot_image(image):
+    image = convert_and_reshape(image)
 
     # plot the sample
     fig = plt.figure
     plt.imshow(image, cmap='gray')
     plt.show()
+    
     print(type(image))
     print(image)
+
+
+if __name__ == "__main__":
+    d = training_data()
+    d.pop_image()
+    plot_image(d.pop_image())
+
     #Verify we iterate correctly:
     while d.has_next_image():
         if d.index % 5000 == 1:
