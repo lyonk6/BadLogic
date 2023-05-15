58c1686fc8a07b2f3decfa31681f30bfa185a081
lyonk6 <fatstrat389@gmail.com>
2023-05-15 15:06:40 -0700

    Dropped dataset.

--- demos/tensorflow/character_RNN.py
+++ demos/tensorflow/character_RNN.py
@@ -26,46 +26,18 @@ dataset_size = tokenizer.document_count # total chars
 # list: [a], ndarray: a
 #encoded = np.array(tokenizer.texts_to_sequences([shakespeare_text])) - 1
 encoded = np.array(tokenizer.texts_to_sequences([list(shakespeare_text)])) - 1
-print("This is the time of the encoded input:", type(encoded))
+print("This is the type of the encoded input:", type(encoded))
 print("This is shape of the encoded input:", encoded.shape)
 # Demonstrate how to convert text to and from with the Tokenizer:
-print(
-    tokenizer.texts_to_sequences(["First"]))
-print(
-    tokenizer.sequences_to_texts([[20, 2, 8, 3]]))
-print(
-    type(tokenizer.texts_to_sequences(["First"])))
 
+"""
 for c in tokenizer.word_index:
     print("The character '", c, "' has a value of: ", tokenizer.texts_to_sequences([c])[0][0])
 
+"""
 
 
-# Splitting Sequential Data:
-train_size = dataset_size * 90 // 100
-print("This is how big our training size is: ", train_size)
-dataset = tf.data.Dataset.from_tensor_slices(encoded[:train_size])
-print("Dataset type: ", type(dataset))
-#Creating multiple windows.
-n_steps = 100
-window_length = n_steps + 1 
-batch_size = 32
-dataset = tf.data.Dataset.from_tensor_slices(encoded[:train_size])
-dataset = dataset.window(window_length, shift=1, drop_remainder=True)
-dataset = dataset.flat_map(lambda window: window.batch(window_length))
-dataset = dataset.batch(1)
-np.random.seed(42)
-tf.random.set_seed(42)
-dataset = dataset.shuffle(10000).batch(batch_size)
-dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
-dataset = dataset.map(lambda x_batch, y_batch: (tf.one_hot(x_batch, depth=max_id), y_batch))
-dataset = dataset.batch(3).prefetch(1)
-
-
-print("Here is the dataset:", dataset)
-
-for x_batch, y_batch in dataset.take(1):
-    print(x_batch.shape, y_batch.shape)
+print(shakespeare_text[:148])
 
 
 """
