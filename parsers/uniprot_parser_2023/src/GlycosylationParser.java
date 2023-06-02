e192d5a08fc8bbd356dac65f6deee8c727679e37
lyonk6 <fatstrat389@gmail.com>
2023-06-01 18:05:21 -0700

    Glycosylation parser completed.

--- parsers/uniprot_parser_2023/src/GlycosylationParser.java
+++ parsers/uniprot_parser_2023/src/GlycosylationParser.java
@@ -49,6 +49,7 @@ public class GlycosylationParser {
                     uniprotFeatureType=uniprotFeatureType.toLowerCase().strip().substring(6, uniprotFeatureType.length()-1);
 
                     if (uniprotFeatureType.equals("glycosylation site")){
+                        motif.description=event.asStartElement().getAttributeByName(new QName("description")).toString();
                         parseGlycosylationEntries(reader, writer, motif);
                     }
                 }
@@ -61,15 +62,25 @@ public class GlycosylationParser {
     }
 
     private static void parseGlycosylationEntries(XMLEventReader reader, BufferedWriter writer, Minimotif motif) throws XMLStreamException {
+        motif.description=motif.description.trim().substring(13, motif.description.length()-1);
+        String[] motifDescriptionArray =  motif.description.split(";");
+        motif.motifType = motifDescriptionArray[0].toLowerCase().trim();
+        for(String s: motifDescriptionArray){
+            if (s.trim().startsWith("by ")){
+                motif.motifTarget = motifDescriptionArray[1].trim().substring(3, motifDescriptionArray[1].trim().length());
+                break;
+            }
+        }
         XMLEvent event_1, event_2, event_3, event_4, event_5, event_6, event_7;
         String motifType, motifTarget, sPosition;
         try{
             event_1 = reader.nextEvent();  // linefeed
             event_2 = reader.nextEvent();  // Start "location"
-            event_3 = reader.nextEvent();  // linefeed
-            event_4 = reader.nextEvent();  // Start "position"
-            sPosition = event_4.asStartElement().getAttributeByName(new QName("position")).toString();
-            writer.write(motif.accessionNumber + '`' + motif.motifType + '`' + motif.motifTarget + '`' + sPosition + "\n");writer.write("");
+            //event_3 = reader.nextEvent();  // linefeed
+            //event_4 = reader.nextEvent();  // Start "position"
+            //sPosition = event_4.asStartElement().getAttributeByName(new QName("position")).toString();
+            motif.position = getModifiedPosition(reader);
+            writer.write(motif.accessionNumber + '`' + motif.motifType + '`' + motif.motifTarget + '`' + motif.position + "\n");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (NullPointerException npe) {
@@ -81,5 +92,30 @@ public class GlycosylationParser {
              nfe.printStackTrace();
              System.exit(1);
          }
+    }
+
+    private static int getModifiedPosition(XMLEventReader reader) throws XMLStreamException {
+        //System.out.println("Getting modified position.");
+        String event_2; //, event_1;
+        int position = -1;
+        reader.nextEvent(); // linefeed
+        XMLEvent e = reader.nextEvent();
+        try{
+            //event_1 = e.asStartElement().getName().getLocalPart(); // start element
+            event_2 = e.asStartElement().getAttributeByName(new QName("position")).toString();
+            event_2 = event_2.substring(10, event_2.length()-1);   // Position
+            //System.out.println(event_1 + ": " + event_2);
+
+            position = Integer.parseInt(event_2);
+        } catch (NullPointerException npe) {
+           System.err.println("This Post-translational modification is poorly formed.");
+           npe.printStackTrace();
+           System.exit(1);
+        } catch (NumberFormatException nfe) {
+            System.err.println("This position is not a number!");
+            nfe.printStackTrace();
+            System.exit(1);
+        }
+        return position;
     }
 }
