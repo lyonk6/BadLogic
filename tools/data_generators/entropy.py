4d3b3a45964563b4f9a87a49cf317a59036847c5
lyonk6 <fatstrat389@gmail.com>
2023-06-10 14:53:40 -0700

    plot p log 1-p

---
+++ tools/data_generators/entropy.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def natural_entropy(y):
+    return -np.sum(y*np.log(y))
+
+def cross_entropy(y_hat, y):
+    return -np.sum(y*np.log(y_hat))
+
+y_hat = np.array([0.2, 0.3, 0.5])
+y = np.array([0.1, 1, 0.5])
+
+def strange_function():
+    """
+    What happens when we plot p log q when p = 1-q 
+     for: 0 < p <= 1
+    """
+    x = np.linspace(0, 1, 100, endpoint=False)[1:]
+    y = np.flip(x)
+    z = np.zeros(x.shape)
+    count = 0
+    for n in x:
+        z[count] = x[count] * np.log(y[count])
+        #print("type:", type(n), " value:", n)
+        count = count + 1
+    return np.array([x,y,z])
+
+
+
+print("cross entropy: ", cross_entropy(y_hat, y), "   inverse: ", cross_entropy(y, y_hat))
+print("self entropy: ", cross_entropy(y, y), "   inverse: ", cross_entropy(y_hat, y_hat))
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    a = strange_function()
+    
+    print("self entropy: ", cross_entropy(a[0], a[0]))
+    plt.scatter(a[0], a[2], alpha=0.5)
+    plt.scatter(a[0], a[1], alpha=0.5)
+    plt.show()
