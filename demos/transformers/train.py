928519fae3f630b59a0e64f9d32e0a2106f81405
lyonk6 <fatstrat389@gmail.com>
2023-06-15 14:14:12 -0700

    Data loader is defined.

--- demos/transformers/train.py
+++ demos/transformers/train.py
@@ -26,7 +26,7 @@ print(decode(encode("hii there")))
 import torch
 data = torch.tensor(encode(text), dtype=torch.long)
 print(data.shape, data.dtype)
-print(data[:1000])
+#print(data[:1000])
 
 # create training and testing datasets using a 90% split. We are 
 # being ambitious here and trying to see if our network can make
@@ -36,17 +36,49 @@ train_data = data[:n]
 val_data   = data[n:]
 """ Pick up at 16 minutes """
 
-## Building/Understanding the Data Loader
-# select a block size of 8 characters:
+### Building the Data Loader
+# Select a block size of 8 characters. Ensure that both 'x' and
+# 'y' are of length 8. Since this is a prediction model the 
+# target data (y) should be one character ahead of the input.
+# 
 block_size=8
 x = train_data[:block_size]
 y = train_data[1:block_size+1]
 for t in range(block_size):
     context = x[:t+1]
     target  = y[t]
-    
+    print(f"when input is {context} the target: {target}")
 
-# The Bigram Language Model
+# Create a "batch dimension" whose only purpose is to aid us in
+# processing this data in parallel. 
+torch.manual_seed(1337)
+batch_size = 4 # sequences to be processed in parallel.
+block_size = 8 # How big are the input/output vectors?
+
+# This function grabs a random chunk from either a training
+# set or a validation set.
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data)-block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[1+i:1+i+block_size] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print("inputs:  ", xb.shape)
+print("outputs: ", yb.shape)
+print(xb)
+print(yb)
+
+print('----')
+
+for b in range(batch_size):
+    for t in range(block_size):
+        context = xb[b, :t+1]
+        target  = yb[b,t]
+        print(f"When the input is {context.tolist()} the target: {target}")
+
+### The Bigram Language Model
 
 # Attention
 
