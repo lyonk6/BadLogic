257966ff8b41750f4b2312ef86c3e2695dca5b8b
lyonk6 <fatstrat389@gmail.com>
2023-06-17 17:00:52 -0700

    Class exists to host protein metadata.

---
+++ parsers/fasta_parser/ProteinMetaData.java
@@ -0,0 +1,33 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;  
+
+public class ProteinMetaData {
+    String id;
+    String common_name;
+    String species;
+    String species_id;
+    
+
+    public ProteinMetaData(String s){
+        String[] s_array = s.split("|");
+        this.id = s_array[1].trim();
+
+        //Match the common name:  FPG_DESHD
+        this.common_name = parseWithPattern("\\w+", s_array[2]);
+
+        //Match the species:  OS=Desulfitobacterium hafniense 
+        this.species    = parseWithPattern("OS=\\w+ \\w+", s_array[2]);
+
+        //Match the species:  OX=272564
+        this.species_id = parseWithPattern("OX=\\d+", s_array[2]);
+    }
+
+    private static String parseWithPattern(String pattern, String header){
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(header);
+        if (m.groupCount() > 0)
+            return m.group(0);
+        else
+            return "unknown";
+    }
+}
