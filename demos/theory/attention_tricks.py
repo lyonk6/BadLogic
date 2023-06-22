8fcd815414e41aab4aa2d06e5ba40d4f2e464716
lyonk6 <fatstrat389@gmail.com>
2023-06-22 11:38:09 -0700

    Trying to grok attention?

---
+++ demos/theory/attention_tricks.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+torch.manual_seed(42)
+
+B, T, C = 4, 8, 2 # Batch, Time, Channel
+x = torch.rand(B, T, C)
+print(x)
+print(x[1,:3])
+
+# TODO What are we doing here?
+xbow = torch.zeros(B, T, C)
+for b in range(B):
+    for t in range(T):
+        xprev = x[b,:t+1]
+        xbow[b,t] = torch.mean(xprev, 0)
+
+#print(xbow)
+
+a = torch.tril(torch.ones(3,3))
+# TODO What does keepdim do?
+a = a/torch.sum(a,1, keepdim=True)
+print(a)
