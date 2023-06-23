9b09612ac1761eff5fc8cc01fd20853eac64f142
lyonk6 <fatstrat389@gmail.com>
2023-06-23 14:11:58 -0700

    moved

---
+++ demos/theory/dice_distributions.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+# Rolls a single die with 'd' sides and return the value
+def roll_die(d):
+    if d > 100:
+        return -1
+    else:
+        return random.randint(1, d)
+
+# Roll 'n' dice with 'd' sides and return their sum
+def roll(n, d):
+    sum = 0
+    for _ in range(n):
+        sum = sum + roll_die(d)
+    return sum
+
+# Roll 'n' dice with 'd' sides and return the sum of 's' samples.
+def roll_distribution(n, d, s=1000):
+    print("n dice: ", n, "   sides:", d, "  samples:", s)
+    l = []
+    for _ in range(s):
+        l.append(roll(n,d))
+    return np.array(l, dtype='i')
+
+def plot_hist(rolls):
+    min_val = np.min(rolls)
+    max_val = np.max(rolls)
+    bin = np.arange(start=min_val - 1 , stop=max_val + 2, step=1, dtype='i')
+    plt.xlim([0, max_val+2])
+    plt.hist(rolls, bins=bin, edgecolor='white')
+    plt.show()
+    plt.cla()
+
+
+
+if __name__ == '__main__':
+    random.seed(42)       
+    plot_hist(roll_distribution(1, 6))
+    plot_hist(roll_distribution(2, 6))
+    plot_hist(roll_distribution(3, 6))
+    plot_hist(roll_distribution(4, 6))
+    plot_hist(roll_distribution(5, 6))
+    plot_hist(roll_distribution(6, 6))
+    plot_hist(roll_distribution(7, 6))
+    plot_hist(roll_distribution(8, 6))
+
+
