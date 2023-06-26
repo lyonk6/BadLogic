19e6eace9e7f87b453fab0ffc02e30e157f93017
lyonk6 <fatstrat389@gmail.com>
2023-06-26 15:58:40 -0700

    Better comments.

--- maths/theory/dice_distributions.py
+++ maths/theory/dice_distributions.py
@@ -2,32 +2,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-# Rolls a single die with 'd' sides and return the value
 def roll_die(d):
+    # Rolls a single die with 'd' sides and return the value.
     if d > 100:
         return -1
     else:
         return random.randint(1, d)
 
-# Roll 'n' dice with 'd' sides and return their sum
 def roll(n, d):
+    #Roll 'n' dice with 'd' sides and return their sum.
     sum = 0
     for _ in range(n):
         sum = sum + roll_die(d)
     return sum
 
-# Roll 'n' dice with 'd' sides and return the sum of 's' samples.
 def roll_distribution(n, d, s=1000):
-    print("n dice: ", n, "   sides:", d, "  samples:", s)
+    """
+    Roll 'n' dice with 'd' sides and return the sums of 's' samples.
+         n dice:  50    sides: 2   samples: 1000
+        [69, 75, 69, 79, 77, 74, 67, 76, 77, 73, 76, 73, 73, 78,  ...
+    """
+    # print("n dice: ", n, "   sides:", d, "  samples:", s)
     l = []
     for _ in range(s):
         l.append(roll(n,d))
     return np.array(l, dtype='i')
 
 def plot_hist(rolls):
+    """
+    # print(rolls)
+    #    [69, 75, 69, 79, 77, 74, 67, 76, 77, 73, 76, 73, 73, 78,  ...
+    """
     min_val = np.min(rolls)
     max_val = np.max(rolls)
     bin = np.arange(start=min_val - 1 , stop=max_val + 2, step=1, dtype='i')
+    # Include x=0 to illustrate how the mean changes. 
     plt.xlim([0, max_val+2])
     plt.hist(rolls, bins=bin, edgecolor='white')
     plt.show()
