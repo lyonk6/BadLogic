a0710f460a35d19d3284dadd189f5bbd1b47f848
lyonk6 <fatstrat389@gmail.com>
2023-06-25 17:51:58 -0700

    Motif maker finds motif sequences for uniprot minimotifs.

--- parsers/fasta_parser/src/Minimotif.java
+++ parsers/fasta_parser/src/Minimotif.java
@@ -27,26 +27,4 @@ public class Minimotif {
                startPosition + '`' + 
                endPosition;
     }
-
-    public static Minimotif fromString(String s){
-        Minimotif m = new Minimotif();
-        try{
-            s_array = s.split("`");
-            m.accessionNumber  = s_array[0];
-            m.sequence         = s_array[1];
-            m.description      = s_array[2];
-            m.motifTarget      = s_array[3];
-            m.uniprotType      = s_array[4];
-            m.modifiedPosition = Integer.parseInt(s_array[5]);
-            m.startPosition    = Integer.parseInt(s_array[6]);
-            m.endPosition      = Integer.parseInt(s_array[7]);
-        } catch(NullPointerError npe){
-            npe.printStackTrace()
-            System.exit(1)
-        } catch(NumberFormatException nfe){
-            nfe.printStackTrace()
-            System.exit(1)
-        }
-        return m;
-    }
 }
--- parsers/fasta_parser/src/MotifMaker.java
+++ parsers/fasta_parser/src/MotifMaker.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 public class MotifMaker {
     public static void main(String[] args) {
         String fastaFilePath = "data/uniprot_sprot.fasta";
+        String motifTestPath = "sample_uniprot_motifs.txt";
         HashMap<String, String> fastaMap = FastaParser.parseFastaFile(fastaFilePath);
 
 
@@ -19,15 +20,78 @@ public class MotifMaker {
             System.out.println();
             break;
         }
+
+        motifSequenceSearcher(motifTestPath, fastaMap);
+
     }
 
-    public static void motifSequenceSearcher(String file){
+    public static void motifSequenceSearcher(String file, HashMap<String, String> fastaMap){
         try{        
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = "";
+            Minimotif motif;
+            int missingMotifs=0;
+            while ((line = reader.readLine()) != null) {
+                motif = fromString(line);
+                if(fastaMap.get(motif.accessionNumber) != null){
+                    findSequence(motif, fastaMap.get(motif.accessionNumber));
+                } else {
+                    System.out.println("No protein found for accession: " + motif.accessionNumber);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            System.exit(1)
+            System.exit(1);
         }
+    }
+
+    public static void findSequence(Minimotif m, String protein){
+        /*
+           int modifiedPosition;
+           int startPosition;
+           int endPosition;
+        */
+       if(protein.length() <=15){
+            m.sequence = protein;
+       }
+
+       if(m.modifiedPosition != -1){
+            if(m.modifiedPosition < 7){
+                m.sequence = "<" + protein.substring(0, m.modifiedPosition + 7);
+            } else 
+            if(m.modifiedPosition > protein.length()-7){
+                m.sequence = protein.substring(0, m.modifiedPosition + 7) + ">";
+            } else {
+                m.sequence = protein.substring(m.modifiedPosition -8 , m.modifiedPosition + 7);
+            }
+       } else {
+            try{
+                m.sequence = protein.substring(m.startPosition-1, m.endPosition);
+            } catch (IndexOutOfBoundsException iobe){
+                System.out.println("Error fetching motif. IndexOutOfBoundsException: " + m.toString());
+            }
+       }
+    }
+
+    public static Minimotif fromString(String s){
+        Minimotif m = new Minimotif();
+        s = s.trim();
+        try{
+            String[] s_array = s.split("`");
+            m.accessionNumber  = s_array[0];
+            m.description      = s_array[1];
+            m.motifTarget      = s_array[2];
+            m.uniprotType      = s_array[3];
+            m.modifiedPosition = Integer.parseInt(s_array[4]);
+            m.startPosition    = Integer.parseInt(s_array[5]);
+            m.endPosition      = Integer.parseInt(s_array[6]);
+        } catch(NullPointerException npe){
+            npe.printStackTrace();
+            System.exit(1);
+        } catch(NumberFormatException nfe){
+            nfe.printStackTrace();
+            System.exit(1);
+        }
+        return m;
     }
 }
